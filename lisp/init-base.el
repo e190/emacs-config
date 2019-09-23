16c63639df4fe4a8a7df0c1d66e8866540107e3e
js.wang <js.wang@mobiletek.cn>
2019-09-23 10:04:25 +0800

    display mode-line size indication

--- lisp/init-base.el
+++ lisp/init-base.el
@@ -35,6 +35,24 @@
   (save-place-mode)
   (setq save-place-file (concat shadow-cache-dir "/places")))
 
+(use-package simple
+  :ensure nil
+  :hook ((window-setup . size-indication-mode)
+         ((prog-mode markdown-mode conf-mode) . enable-trailing-whitespace))
+  :init
+  (setq column-number-mode t
+        line-number-mode t
+        ;; kill-whole-line t               ; Kill line including '\n'
+        line-move-visual nil
+        track-eol t                     ; Keep cursor at end of lines. Require line-move-visual is nil.
+        set-mark-command-repeat-pop t)  ; Repeating C-SPC after popping mark pops it again
+
+  ;; Visualize TAB, (HARD) SPACE, NEWLINE
+  (setq-default show-trailing-whitespace nil) ; Don't show trailing whitespace by default
+  (defun enable-trailing-whitespace ()
+    "Show trailing spaces and delete on saving."
+    (setq show-trailing-whitespace t)
+    (add-hook 'before-save-hook #'delete-trailing-whitespace nil t)))
 
 (provide 'init-base)
 ;;; config-base.el ends here
